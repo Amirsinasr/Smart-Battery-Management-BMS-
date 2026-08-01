6748e57fa7f36e93bf963659ac811d7c38ab1a9c
Amirsinasr <sahraei2.sina.me@gmail.com>
2026-08-01 10:54:17 +0200

    Setting up pin 17-18 of ESP32

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+#define I2C_SDA 17
+#define I2C_SCL 18
+
+Adafruit_INA219 ina219;
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  
+  Wire.begin(I2C_SDA, I2C_SCL);
+
+  if (!ina219.begin()) {
+    Serial.println("Failed to find INA219 chip. Check your wiring!");
+    while (1) { delay(10); } 
+  }
+  
+  Serial.println("INA219 Found and successfully communicating on pins 17 and 18!");
 }
 
 void loop() {
